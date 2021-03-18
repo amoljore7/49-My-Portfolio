510298d16317978ce99dd627d69e00df8804006c
amol <amol.jore@gslab.com>
2021-03-18 15:38:53 +0530

    Updates

---
+++ precache-manifest.a5ba111c930fb6543f1a27ad086128aa.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9afcbcdf693a34e2b928e8ec7285e9",
+    "url": "/49-Amol-Jore-Portfolio/index.html"
+  },
+  {
+    "revision": "aa4aace176d2edce3333",
+    "url": "/49-Amol-Jore-Portfolio/static/css/main.8c1a3127.chunk.css"
+  },
+  {
+    "revision": "10bd8615b6c5834007af",
+    "url": "/49-Amol-Jore-Portfolio/static/js/2.688c09cb.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/49-Amol-Jore-Portfolio/static/js/2.688c09cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4aace176d2edce3333",
+    "url": "/49-Amol-Jore-Portfolio/static/js/main.e462a6ac.chunk.js"
+  },
+  {
+    "revision": "357fbaf03e7536acd679",
+    "url": "/49-Amol-Jore-Portfolio/static/js/runtime-main.9f96d71b.js"
+  },
+  {
+    "revision": "20078eabf7beff92697d70cf3335dc40",
+    "url": "/49-Amol-Jore-Portfolio/static/media/GS-Lab-Logo.20078eab.jpg"
+  },
+  {
+    "revision": "c924155285cdba2f57cbd751674c1bb0",
+    "url": "/49-Amol-Jore-Portfolio/static/media/L_ANT_D.c9241552.png"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/49-Amol-Jore-Portfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/49-Amol-Jore-Portfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/49-Amol-Jore-Portfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/49-Amol-Jore-Portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "add239172d50d6ad18d2ac3cea6257a1",
+    "url": "/49-Amol-Jore-Portfolio/static/media/digital-ocean.add23917.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/49-Amol-Jore-Portfolio/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "09cd28347983de7287a1af0792371087",
+    "url": "/49-Amol-Jore-Portfolio/static/media/docker.09cd2834.png"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/49-Amol-Jore-Portfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/49-Amol-Jore-Portfolio/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/49-Amol-Jore-Portfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "1c0872cb25c8b5b1daf45929da1f663b",
+    "url": "/49-Amol-Jore-Portfolio/static/media/gitHub.1c0872cb.png"
+  },
+  {
+    "revision": "0555bfb7e81f57a1efe6a53c7490abb7",
+    "url": "/49-Amol-Jore-Portfolio/static/media/github-api.0555bfb7.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/49-Amol-Jore-Portfolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "30bd42ea97a2e0454bb1dfbab983eb54",
+    "url": "/49-Amol-Jore-Portfolio/static/media/go-6.30bd42ea.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/49-Amol-Jore-Portfolio/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/49-Amol-Jore-Portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "a7491ce8631676396416895e8412ccc5",
+    "url": "/49-Amol-Jore-Portfolio/static/media/ic_banner.a7491ce8.png"
+  },
+  {
+    "revision": "8575a3badf378c399c61ff792c802362",
+    "url": "/49-Amol-Jore-Portfolio/static/media/ic_mobile_banner.8575a3ba.png"
+  },
+  {
+    "revision": "125a3ce320ae049d156bd7f058f162ee",
+    "url": "/49-Amol-Jore-Portfolio/static/media/ic_profile_img.125a3ce3.jpg"
+  },
+  {
+    "revision": "564fe4503b2b740f09fc2471e0ec1877",
+    "url": "/49-Amol-Jore-Portfolio/static/media/ic_youtube.564fe450.png"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/49-Amol-Jore-Portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "f2902b7b8b68efc108627227bbc57254",
+    "url": "/49-Amol-Jore-Portfolio/static/media/k12.f2902b7b.png"
+  },
+  {
+    "revision": "dc6d95c76b1a3a24f431263e56e74a48",
+    "url": "/49-Amol-Jore-Portfolio/static/media/logow.dc6d95c7.webp"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/49-Amol-Jore-Portfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/49-Amol-Jore-Portfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/49-Amol-Jore-Portfolio/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/49-Amol-Jore-Portfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/49-Amol-Jore-Portfolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "125a3ce320ae049d156bd7f058f162ee",
+    "url": "/49-Amol-Jore-Portfolio/static/media/profile.125a3ce3.webp"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/49-Amol-Jore-Portfolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/49-Amol-Jore-Portfolio/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/49-Amol-Jore-Portfolio/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/49-Amol-Jore-Portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/49-Amol-Jore-Portfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/49-Amol-Jore-Portfolio/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "4a3dff38645f4aa8212c2c6a319da256",
+    "url": "/49-Amol-Jore-Portfolio/static/media/shopingHub.4a3dff38.png"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/49-Amol-Jore-Portfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/49-Amol-Jore-Portfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/49-Amol-Jore-Portfolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "e79a42b9281a3b30f1302d5452f9884f",
+    "url": "/49-Amol-Jore-Portfolio/static/media/styled-components.e79a42b9.svg"
+  },
+  {
+    "revision": "28d1def016530838afa3da18d6500af2",
+    "url": "/49-Amol-Jore-Portfolio/static/media/tic-tac-toe.28d1def0.png"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/49-Amol-Jore-Portfolio/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "448f9aa058502af8f8e23e2e05597106",
+    "url": "/49-Amol-Jore-Portfolio/static/media/weatherAPP.448f9aa0.png"
+  }
+]);
